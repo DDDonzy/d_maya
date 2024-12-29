cf83ff1b2786746cbf0c4f1e87a516dee11d95c5
DDDonzy <donzy.xu@outlook.com>
2024-12-30 01:15:47 +0800

    update namespace

---
+++ utils/scene/delete_namespace.py
@@ -0,0 +1,19 @@
+# Author:   Donzy.xu
+# CreateTime:   2023/2/16 - 16:29
+# FileName:  namespace.py
+
+
+import maya.cmds as cmds
+
+
+def clear_namespace():
+    system_namespace_list = ['UI', 'shared']
+    namespace_list = cmds.namespaceInfo(":", lon=True)
+    namespace_list = list(set(namespace_list) ^ set(system_namespace_list))
+
+    while len(namespace_list) > 0:
+        cmds.namespace(rm=namespace_list[0], mnr=1)
+        namespace_list = cmds.namespaceInfo(":", lon=True)
+        namespace_list = list(set(namespace_list) ^ set(system_namespace_list))
+
+# clear_namespace()
