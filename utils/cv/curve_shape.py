4a5ea9a76dea1e32b703539123f1319d398ac89f
DDDonzy <donzy.xu@outlook.com>
2024-12-29 21:46:27 +0800

    add mirror cv

--- utils/cv/curve_shape.py
+++ utils/cv/curve_shape.py
@@ -9,19 +9,14 @@ from maya.api import OpenMaya as om
 @dataclass
 class CurveShapeData(yaml.YAMLObject):
     yaml_tag = 'CurveShapeData'
-
     shapeName: str = "shapeName"
-
     lineWidth: int = 0
-
     overrideEnabled: bool = False
     overrideRGBColors: bool = False
     hideOnPlayback: bool = False
-
     overrideColor: int = 0
     overrideDisplayType: int = 0
     overrideColorRGB: list = field(default_factory=list)
-
     shapeSetAttrCmd: str = None
 
     def __post_init__(self):
@@ -53,30 +48,33 @@ class CurveShapeData(yaml.YAMLObject):
                 setattr(self, attr, cmds.getAttr(f"{shape_object}.{attr}"))
         self.shapeSetAttrCmd = get_nurbsCurveType_setAttrCmd(self.shapeName)
 
-    def set_to_shape(self, shape_object=None):
+    def set_to_shape(self, shape_object=None,
+                     setShape: bool = True,
+                     setDrawInfo: bool = True):
         self.check_shape(shape_object)
 
-        for attr in self.attrs:
+        if setDrawInfo:
+            for attr in self.attrs:
+                try:
+                    if attr == "overrideColorRGB":
+                        cmds.setAttr(f"{shape_object}.{attr}", *getattr(self, attr))
+                    else:
+                        cmds.setAttr(f"{shape_object}.{attr}", getattr(self, attr))
+                except:
+                    cmds.warning(f"setAttr '{shape_object}.{attr}' fail.")
+        if setShape:
+            sel_temp = cmds.ls(sl=1)
+            cmds.select(shape_object)
             try:
-                if attr == "overrideColorRGB":
-                    cmds.setAttr(f"{shape_object}.{attr}", *getattr(self, attr))
-                else:
-                    cmds.setAttr(f"{shape_object}.{attr}", getattr(self, attr))
+                mel.eval(self.shapeSetAttrCmd)
             except:
-                cmds.warning(f"setAttr '{shape_object}.{attr}' fail.")
-        sel_temp = cmds.ls(sl=1)
-        cmds.select(shape_object)
-        try:
-            mel.eval(self.shapeSetAttrCmd)
-        except:
-            cmds.warning(f"setAttr '{shape_object}.create' fail.")
-        cmds.select(sel_temp)
+                cmds.warning(f"setAttr '{shape_object}.create' fail.")
+            cmds.select(sel_temp)
 
 
-@dataclass
+@ dataclass
 class CurveData(yaml.YAMLObject):
     yaml_tag = 'CurveData'
-
     transformName: str = "transformName"
     shapes: list = field(default_factory=list)
 
@@ -99,7 +97,9 @@ class CurveData(yaml.YAMLObject):
         for shape in shape_list:
             self.shapes.append(CurveShapeData(shape))
 
-    def set_data(self, transform_obj=None):
+    def set_data(self, transform_obj: str = None,
+                 setShape: bool = True,
+                 setDrawInfo: bool = True):
         if not transform_obj:
             transform_obj = self.transformName
         if not cmds.objExists(transform_obj):
@@ -123,16 +123,18 @@ class CurveData(yaml.YAMLObject):
 
         if len(self.shapes) == len(target_shape_list):
             for i, x in enumerate(self.shapes):
-                self.shapes[i].set_to_shape(target_shape_list[i])
+                self.shapes[i].set_to_shape(target_shape_list[i],
+                                            setShape,
+                                            setDrawInfo)
 
 
 def get_curve_shape(transform_object):
-    """get nurbsCurve shapes 
+    """get nurbsCurve shapes
     Args:
         transform_object (str): transform name
 
     Returns:
-        list: return shape list 
+        list: return shape list
     """
     if not cmds.objExists(transform_object):
         raise RuntimeError(transform_object + "is not exists")
@@ -155,7 +157,7 @@ def get_nurbsCurveType_setAttrCmd(shape_obj):
         shape_obj (str): nurbs curve shape's name
 
     Returns:
-        str: return cmd about set curve shapes 
+        str: return cmd about set curve shapes
     """
     mSel = om.MSelectionList()
     mSel.add(f"{shape_obj}.local")
@@ -165,7 +167,9 @@ def get_nurbsCurveType_setAttrCmd(shape_obj):
     return cmd_str
 
 
-def replace_curve_shape(source, target):
+def replace_curve_shape(source, target,
+                        setShape: bool = True,
+                        setDrawInfo: bool = True):
     """ replace curve's shapes
     Args:
         source (str): source curve object
@@ -181,7 +185,70 @@ def replace_curve_shape(source, target):
 
     cvData = CurveData(source)
     for obj in target:
-        cvData.set_data(obj)
+        cvData.set_data(obj,
+                        setShape=setShape,
+                        setDrawInfo=setDrawInfo)
+
+
+def name_replace(name: str,
+                 replace: list = ["L_", "R_"]):
+    """input a name and replace str
+
+    Args:
+        name (str): _description_
+        replace (list, optional): _description_. Defaults to ["L_", "R_"].
+    Returns:
+        str: replaced name
+    """
+    if replace[0] in name:
+        name = name.replace(*replace)
+    elif replace[1] in name:
+        replace.reverse()
+        name = name.replace(*replace)
+    else:
+        raise RuntimeError(f"object '{name}' replace error,'{replace[0]}' or '{replace[1]}' not in it.")
+    return name
+
+
+def mirror_curve_shape(object: list = [],
+                       replace: list = ["L_", "R_"],
+                       setShape: bool = True,
+                       setDrawInfo: bool = False):
+    """ Mirror the shape of one side to the other side.
+
+    Args:
+        object (list, optional): Source transform object list
+        replace (list, optional): Replace str[0] with str[1] in the transform object name.
+        setShape (bool, optional): If False will be pass mirror shape.
+        setDrawInfo (bool, optional): If False will be pass mirror shape draw information.
+    """
+
+    if type(object) is str:
+        object = [object]
+
+    for obj in object:
+        # obj is true
+        if not cmds.objExists(obj):
+            om.MGlobal.displayInfo(f"Can not find '{obj}'.")
+            continue
+        # otherSize is true
+        try:
+            otherSide_obj = name_replace(obj, replace)
+        except RuntimeError:
+            om.MGlobal.displayInfo(f"Can not find '{otherSide_obj}'.")
+            continue
+        # transf cv shape data
+        cvData = CurveData(obj)
+        cvData.set_data(otherSide_obj, setShape=setShape, setDrawInfo=setDrawInfo)
+        # .cv[*] * -1
+        obj_shape_list = get_curve_shape(obj)
+        otherSide_obj_shape_list = get_curve_shape(otherSide_obj)
+        for shape_i, shape in enumerate(obj_shape_list):
+            pos_list = cmds.xform(f"{shape}.cv[*]", q=1, t=1, ws=1)
+            pos_ary = [pos_list[i:i+3] for i in range(0, len(pos_list), 3)]
+            for cv_i, cv_pos in enumerate(pos_ary):
+                cv_pos[0] *= -1
+                cmds.xform(f"{otherSide_obj_shape_list[shape_i]}.cv[{cv_i}]", t=cv_pos, ws=1)
 
 
 def export_curve_data(cv_list=None):
@@ -206,7 +273,7 @@ def export_curve_data(cv_list=None):
     cmds.inViewMessage(amg=message, pos='midCenterBot', fade=True)
 
 
-def import_curve_data(cv_list=None):
+def import_curve_data():
     path = cmds.fileDialog2(dialogStyle=2, caption="Import nurbsCurve data", fileFilter="YAML file(*.yaml)", fileMode=1)[0]
     with open(path, "r") as f:
         data_list = yaml.unsafe_load(f)
@@ -222,3 +289,4 @@ def import_curve_data(cv_list=None):
 # export_curve_data()
 # import_curve_data()
 # replace_curve_shape(cmds.ls(sl=1)[0],cmds.ls(sl=1)[1:])
+# mirror_curve_shape(cmds.ls(sl=1), setShape=1, setDrawInfo=0)
--- utils/cv/curve_shape_py2.py
+++ utils/cv/curve_shape_py2.py
@@ -47,23 +47,28 @@ class CurveShapeData(yaml.YAMLObject):
                 setattr(self, attr, cmds.getAttr("{0}.{1}".format(shape_object, attr)))
         self.shapeSetAttrCmd = get_nurbsCurveType_setAttrCmd(self.shapeName)
 
-    def set_to_shape(self, shape_object=None):
+    def set_to_shape(self, shape_object=None,
+                     setShape=True,
+                     setDrawInfo=True):
         self.check_shape(shape_object)
-        for attr in self.attrs:
+        if setDrawInfo:
+            for attr in self.attrs:
+                try:
+                    if attr == "overrideColorRGB":
+                        cmds.setAttr("{0}.{1}".format(shape_object, attr), *getattr(self, attr))
+                    else:
+                        cmds.setAttr("{0}.{1}".format(shape_object, attr), getattr(self, attr))
+                except:
+                    cmds.warning("setAttr '{0}.{1}' fail".format(shape_object, attr))
+            
+        if setShape:
+            sel_temp = cmds.ls(sl=1)
+            cmds.select(shape_object)
             try:
-                if attr == "overrideColorRGB":
-                    cmds.setAttr("{0}.{1}".format(shape_object, attr), *getattr(self, attr))
-                else:
-                    cmds.setAttr("{0}.{1}".format(shape_object, attr), getattr(self, attr))
+                mel.eval(self.shapeSetAttrCmd)
             except:
-                cmds.warning("setAttr '{0}.{1}' fail".format(shape_object, attr))
-        sel_temp = cmds.ls(sl=1)
-        cmds.select(shape_object)
-        try:
-            mel.eval(self.shapeSetAttrCmd)
-        except:
-            cmds.warning("setAttr '{}.create' fail".format(shape_object))
-        cmds.select(sel_temp)
+                cmds.warning("setAttr '{}.create' fail".format(shape_object))
+            cmds.select(sel_temp)
 
 
 class CurveData(yaml.YAMLObject):
@@ -89,7 +94,9 @@ class CurveData(yaml.YAMLObject):
         for shape in shape_list:
             self.shapes.append(CurveShapeData(shape))
 
-    def set_data(self, transform_obj=None):
+    def set_data(self, transform_obj=None,
+                 setShape=True,
+                 setDrawInfo=True):
         if not transform_obj:
             transform_obj = self.transformName
         if not cmds.objExists(transform_obj):
@@ -114,7 +121,7 @@ class CurveData(yaml.YAMLObject):
 
         if len(self.shapes) == len(target_shape_list):
             for i, x in enumerate(self.shapes):
-                self.shapes[i].set_to_shape(target_shape_list[i])
+                self.shapes[i].set_to_shape(target_shape_list[i], setShape=setShape, setDrawInfo=setDrawInfo)
 
 
 def get_curve_shape(transform_object):
@@ -209,6 +216,65 @@ def import_curve_data(cv_list=None):
     message = '<hl> Import successful </hl>'
     cmds.inViewMessage(amg=message, pos='midCenterBot', fade=True)
 
+
+def name_replace(name, replace=["L_", "R_"]):
+    """input a name and replace str
+
+    Args:
+        name (str): _description_
+        replace (list, optional): _description_. Defaults to ["L_", "R_"].
+    Returns:
+        str: replaced name
+    """
+    if replace[0] in name:
+        name = name.replace(replace[0], replace[1])
+    elif replace[1] in name:
+        replace.reverse()
+        name = name.replace(replace[0], replace[1])
+    else:
+        raise RuntimeError("object '{0}' replace error,'{1}' or '{2}' not in it.".format(name, replace[0], replace[1]))
+    return name
+
+
+def mirror_curve_shape(object=[], replace=["L_", "R_"], setShape=True, setDrawInfo=False):
+    """ Mirror the shape of one side to the other side.
+
+    Args:
+        object (list, optional): Source transform object list
+        replace (list, optional): Replace str[0] with str[1] in the transform object name.
+        setShape (bool, optional): If False will be pass mirror shape.
+        setDrawInfo (bool, optional): If False will be pass mirror shape draw information.
+    """
+
+    if isinstance(object, (str, unicode)):
+        object = [object]
+
+    for obj in object:
+        # obj exists check
+        if not cmds.objExists(obj):
+            om.MGlobal.displayInfo("Can not find '{0}'.".format(obj))
+            continue
+
+        # Try to replace the name to find the other side object
+        try:
+            otherSide_obj = name_replace(obj, replace)
+        except RuntimeError:
+            om.MGlobal.displayInfo("Can not find '{0}'.".format(obj))
+            continue
+
+        # Get curve data and apply transformation
+        cvData = CurveData(obj)
+        cvData.set_data(otherSide_obj, setShape=setShape, setDrawInfo=setDrawInfo)
+
+        # Mirror curve points by inverting X-axis
+        obj_shape_list = get_curve_shape(obj)
+        otherSide_obj_shape_list = get_curve_shape(otherSide_obj)
+        for shape_i, shape in enumerate(obj_shape_list):
+            pos_list = cmds.xform("{0}.cv[*]".format(shape), q=1, t=1, ws=1)
+            pos_ary = [pos_list[i:i+3] for i in range(0, len(pos_list), 3)]
+            for cv_i, cv_pos in enumerate(pos_ary):
+                cv_pos[0] *= -1  # Invert the X-axis for mirroring
+                cmds.xform("{0}.cv[{1}]".format(otherSide_obj_shape_list[shape_i], cv_i), t=cv_pos, ws=1)
 
 # export_curve_data()
 # import_curve_data()
