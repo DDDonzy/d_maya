b242b34bbfa3a5d3583a77f88fde253a3946565b
DDDonzy <donzy.xu@outlook.com>
2025-01-12 22:21:56 +0800

    update bs tools

---
+++ utils/bs/selectInputTargetGeometry.py
@@ -0,0 +1,24 @@
+from maya.api import OpenMaya as om
+from maya import cmds
+
+
+def selectInputTargetGeometry():
+    objList = []
+    bsNode = cmds.ls(sl=1)
+    if not bsNode:
+        return
+    bsNode = bsNode[0]
+    bsTargetList = cmds.channelBox("mainChannelBox", q=1, sha=1) or []
+
+    for x in bsTargetList:
+        mSel = om.MSelectionList()
+        mSel.add(f"{bsNode}.{x}")
+        mPlug = mSel.getPlug(0)
+        logicalIndex = mPlug.logicalIndex()
+        inputTargetAttr = f"{bsNode}.inputTarget[0].inputTargetGroup[{logicalIndex}].inputTargetItem[6000].inputGeomTarget"
+        objList.extend(cmds.listConnections(inputTargetAttr, s=1, d=0))
+    cmds.select(objList)
+
+
+if __name__ == "__main__":
+    selectInputTargetGeometry()
