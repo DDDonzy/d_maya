648452310a990f111b9fd81b7e330616119da914
DDDonzy <donzy.xu@outlook.com>
2025-01-16 02:51:28 +0800

    xx

--- utils/createBase.py
+++ utils/createBase.py
@@ -7,28 +7,43 @@ def CreateNode(*args, **kwargs):
     return cmds.createNode(*args, **kwargs)
 
 
+def createContainer(name: str,
+                    assetType: str = 'dagContainer',
+                    addNode: list = [],
+                    blackBox: bool = False,
+                    icon: str = None,
+                    publishAttrData: dict = None):
+
+    container = cmds.createNode(assetType, name=name, ss=1)
+    cmds.container(container, e=1, an=addNode)
+    cmds.setAttr(f"{container}.blackBox", blackBox) if blackBox else None
+    cmds.setAttr(f"{container}.iconName", icon) if icon else None
+    cmds.setAttr(f"{container}.viewMode", 0)
+    if publishAttrData:
+        AssetCallBack.publishAssetAttr(name=container, publishAttrData=publishAttrData)
+
+
+def publishAssetAttr(name: str = None, publishAttrData: dict = None):
+    if publishAttrData and isinstance(publishAttrData, dict):
+        for k, v in publishAttrData.items():
+            cmds.container(name, e=1, publishAndBind=[v, k])
+
+
+
 class AssetCallBack:
     callBack_instance = []
 
-    def __init__(self,
-                 name: str = "",
-                 assetType: str = "container",
-                 blackBox: bool = False,
-                 icon: str = None,
-                 publishAttrData: dict = {}):
-        self.name = name
+    def __init__(self):
         self.addNode: list[str] = []
-
-        self.assetType = assetType
-        self.blackBox = blackBox
-        self.icon = icon
-        self.publishAttrData = publishAttrData
-
         self.addCallBackID: int = 0
         self.removeCallBackID: int = 0
-
+        self.__enter__()
+        
+    def __del__(self):
+        self.removeNodeCallBack()
+        
     def __repr__(self):
-        return str(self.name)
+        return str(self.addNode)
 
     def __enter__(self):
         self.addCallBack()
@@ -36,11 +51,6 @@ class AssetCallBack:
 
     def __exit__(self, exc_type, exc_val, exc_tb):
         self.removeNodeCallBack()
-        AssetCallBack.createContainer(name=self.name,
-                                      assetType=self.assetType,
-                                      addNode=self.addNode,
-                                      blackBox=self.blackBox,
-                                      publishAttrData=self.publishAttrData)
 
     def addCallBack(self):
         self.base_callBack_instance: list[AssetCallBack] = AssetCallBack.callBack_instance.copy()
@@ -60,40 +70,24 @@ class AssetCallBack:
         for baseCallback in self.base_callBack_instance:
             baseCallback.addCallBackID = om.MDGMessage.addNodeAddedCallback(AssetCallBack.addNodeFunction, "dependNode", baseCallback)
             baseCallback.removeCallBackID = om.MDGMessage.addNodeRemovedCallback(AssetCallBack.removeNodeFunction, "dependNode", baseCallback)
+            self.addCallBackID = baseCallback.addCallBackID
+            self.removeCallBackID = baseCallback.removeCallBackID
+            AssetCallBack.callBack_instance.append(baseCallback)
 
     @staticmethod
     def addNodeFunction(mObj, thisClass):
         node = om.MFnDependencyNode(mObj)
         node_name = node.name()
         thisClass.addNode.append(node_name)
+        print(node_name)
 
     @staticmethod
     def removeNodeFunction(mObj, thisClass):
         node = om.MFnDependencyNode(mObj)
         node_name = node.name()
         thisClass.addNode.remove(node_name)
+        print(node_name)
 
-    @staticmethod
-    def createContainer(name: str,
-                        assetType: str = 'dagContainer',
-                        addNode: list = [],
-                        blackBox: bool = False,
-                        icon: str = None,
-                        publishAttrData: dict = None):
-
-        container = cmds.createNode(assetType, name=name, ss=1)
-        cmds.container(container, e=1, an=addNode)
-        cmds.setAttr(f"{container}.blackBox", blackBox) if blackBox else None
-        cmds.setAttr(f"{container}.iconName", icon) if icon else None
-        cmds.setAttr(f"{container}.viewMode", 0)
-        if publishAttrData:
-            AssetCallBack.publishAssetAttr(name=container, publishAttrData=publishAttrData)
-
-    @staticmethod
-    def publishAssetAttr(name: str = None, publishAttrData: dict = None):
-        if publishAttrData and isinstance(publishAttrData, dict):
-            for k, v in publishAttrData.items():
-                cmds.container(name, e=1, publishAndBind=[v, k])
 
 
 class CreatorBase():
@@ -102,6 +96,8 @@ class CreatorBase():
     blackBox: bool = False
     icon: str = None
     publishAttrData: dict = {}
+    
+    __CALLBACK_INSTANCE = []
 
     def __init__(self, name='noName'):
         self._pre_init()
