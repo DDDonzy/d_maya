63d79456e76381a7f79f20d08899edc8fb1a3ef5
DDDonzy <donzy.xu@outlool.com>
2025-02-20 18:25:51 +0800

    update to max

--- _gameFace/maxCommand.py
+++ _gameFace/maxCommand.py
@@ -1,9 +1,9 @@
 
 import textwrap
-import face.fit as fit
 import face.control as control
 from maya import cmds
 import yaml
+from face.data.config import *
 
 
 class toMaxCommand():
@@ -16,7 +16,7 @@ class toMaxCommand():
     def uvPin(self, obj, uvPin, faceIndex, vis):
         vis = bool(vis)
         command = textwrap.dedent("""
-                                        
+
         obj = ${0}
         pin = Point()
         pin.name = "{0}_Pin"
@@ -28,7 +28,7 @@ class toMaxCommand():
         pin.isHidden = {3}
         pin.isFrozen = {3}
         pin.parent = obj.parent
-        
+
         attachController = Attachment()
         pin.pos.controller = attachController
         attachController.node = ${1}
@@ -37,10 +37,10 @@ class toMaxCommand():
         keyNew.face = {2}
         keyNew.coord = [0,0]
         AttachCtrl.update attachController
-        
+
         driver = pin
         driven = obj
-        
+
         offsetMatrix = driven.transform * inverse driver.transform
         obj_exp = driven.Transform.controller = transform_script ()
 
@@ -48,7 +48,7 @@ class toMaxCommand():
         obj_exp.addNode "driven" driven
         obj_exp.addConstant "offsetMatrix" offsetMatrix
         obj_exp.script = "offsetMatrix * driver.transform * inverse driven.parent.transform"
-        
+
         """.format(obj, uvPin, faceIndex, vis))
 
         self.command += command
@@ -56,7 +56,7 @@ class toMaxCommand():
     def matrixConstraint(self, driver, driven):
 
         command = textwrap.dedent("""
-                                                    
+
         driver = ${0}
         driven = ${1}
 
@@ -67,14 +67,14 @@ class toMaxCommand():
         obj_exp.addNode "driven" driven
         obj_exp.addConstant "offsetMatrix" offsetMatrix
         obj_exp.script = "offsetMatrix * driver.transform * inverse driven.parent.transform"
-        
+
         """.format(driver, driven))
 
         self.command += command
 
     def limitAll(self, obj):
         command = textwrap.dedent("""
-                                  
+
         obj = ${0}
 
         limit = obj.position.controller.X_Position.controller = float_limit ()
@@ -142,36 +142,102 @@ class toMaxCommand():
         limit.upper_limit_enabled = True
         limit.lower_limit = 1
         limit.lower_limit_enabled = True
-        
+
         """.format(obj))
 
         self.command += command
 
-    def unLimit(self,obj, axis, limit):
-        
+    def unLimit(self, obj, axis, limit):
+
         command = textwrap.dedent("""
-                                  
-        limit = ${0}.position.controller.{1}Position.controller
+        limit = ${0}.position.controller.{1}_Position.controller = linear_float()
+        limit = ${0}.position.controller.{1}_Position.controller = float_limit()
         limit.Enable=True
         limit.upper_limit = {2}
         limit.upper_limit_enabled = True
         limit.lower_limit = {3}
         limit.lower_limit_enabled = True
-        
-        """.format(obj,axis,max(*limit),min(*limit)))
+
+        """.format(obj, axis, max(*limit), min(*limit)))
+        self.command += command
+
+    def sdk(self, driver, mesh, driverValue, drivenValue, axis, index):
+        command = textwrap.dedent("""
+
+        b1 = ${0}.controller.position.controller.{1}_Position.controller
+        bs = ${2}.morpher
+
+        dv_min = {4}
+        dv_max = {5}
+
+        v1 = {6}
+        v2 = {7}
+
+        cont = bs[{3}].controller = Float_Reactor()
+        reactTo cont b1
+
+        createReaction cont
+
+
+        setReactionValue cont 1 dv_min
+        setReactionValue cont 2 dv_max
+
+
+        setReactionState cont 1 v1
+        setReactionState cont 2 v2
+
+
+
+        """.format(driver,
+                   axis.upper(),
+                   mesh,
+                   index,
+                   min(driverValue),
+                   max(driverValue),
+                   drivenValue[driverValue.index(min(driverValue))],
+                   drivenValue[driverValue.index(max(driverValue))]))
         self.command += command
 
 
 command = toMaxCommand()
 
-
+# joint constraint
 all_sk = control.get_allSkinJoint()
 for sk in all_sk:
     ctl = control.ControlData(sk)
     command.matrixConstraint(driver=ctl.loc, driven=sk)
 
+# uvpin -> attachment
 uvPinList = ['Head_uvPinMesh', "Jaw_uvPinMesh", "part_uvPinMesh", "Sec_uvPinMesh"]
 for uvPin in uvPinList:
     data = yaml.load(cmds.getAttr("{}.notes".format(uvPin)))
     for x in data:
         command.uvPin(obj=x["driven"], uvPin=uvPin, faceIndex=(x["meshComponent"][0]/5)*4, vis=False)
+
+# sdk
+data = yaml.load(cmds.getAttr("FaceBridge.notes"))
+for i, x in enumerate(data):
+    if x.driverAttr:
+        axis = x.driverAttr[-1]
+        driverValue = [x.min, x.max]
+        index = i+1
+        driver = x.driverAttr.split(".")[0]
+
+        for mesh in ['Head_uvPinMesh', "Jaw_uvPinMesh", "part_uvPinMesh", "Sec_uvPinMesh"]:
+            command.sdk(driver=driver, mesh=mesh, driverValue=driverValue, drivenValue=[0, 100], axis=axis, index=index)
+
+# limit
+ctl = []
+data = {}
+data = yaml.load(cmds.getAttr("FaceBridge.notes"))
+for i, x in enumerate(data):
+    if x.driverAttr:
+        driver = x.driverAttr.split(".")[0]
+        if driver not in ctl:
+            ctl.append(driver)
+for x in ctl:
+    command.limitAll(x)
+    v = eval("cmds.transformLimits('{}',q=1,tx=1)".format(x))
+    command.unLimit(x, "x", v)
+    v = eval("cmds.transformLimits('{}',q=1,ty=1)".format(x))
+    command.unLimit(x, "y", v)
