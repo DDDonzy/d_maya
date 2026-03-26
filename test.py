18e7d9ed54da23bb82454fafb4c138a67347e1b5
dddonzy <donzy.xu@qq.com>
2026-03-26 11:49:19 +0800

    update

--- test.py
+++ test.py
@@ -1,14 +1,3 @@
-from maya.api import OpenMaya as om
+from maya import utils as mutils
 
-
-def test():
-
-    mSel: om.MSelectionList = om.MGlobal.getActiveSelectionList()
-    if mSel.length() == 0:
-        raise RuntimeError("Please select meshes!")
-    mDag: om.MDagPath = mSel.getDagPath(0).extendToShape()
-    if not mDag.hasFn(om.MFn.kMesh):
-        om.MGlobal.displayError("Please select meshes!")
-        raise RuntimeError("sdafsa")
-
-def
+mutils.executeDeferred()
